514a06f895685e4a9be9289dcab5678993e7e8b5
Lee Hyeonji <lhj5426@naver.com>
2025-04-23 20:51:21 +0900

    jwt code refactoring

--- src/main/java/com/study/study_platform/security/SecurityConfig.java
+++ src/main/java/com/study/study_platform/security/SecurityConfig.java
@@ -1,11 +1,10 @@
-package com.study.study_platform.config; // <-- 여러분의 패키지에 맞게 수정하세요
+package com.study.study_platform.security;
 
-import com.study.study_platform.security.JwtTokenFilter;
-import com.study.study_platform.security.JwtTokenUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -46,26 +45,13 @@ public class SecurityConfig  {
                                 .requestMatchers("/login", "/signup").permitAll()  // 로그인, 회원가입은 모두 접근 허용
                                 .anyRequest().authenticated()  // 그 외 요청은 인증 필요
                 )
-                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class) // JWT 필터를 기본 UsernamePasswordAuthenticationFilter 전에 추가
-
-                .formLogin(formLogin ->
-                        formLogin
-                                .loginPage("/login")  // 로그인 페이지 URL 지정
-                                .loginProcessingUrl("/perform_login")  // 로그인 처리 URL
-                                .permitAll()  // 로그인 페이지는 모든 사용자에게 허용
-                )
-                .httpBasic(httpBasic ->
-                        httpBasic
-                                .realmName("MyApp")  // HTTP Basic 인증에서 사용할 realm 이름
-                );
-
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class); // JWT 필터를 기본 UsernamePasswordAuthenticationFilter 전에 추가
         return http.build();
     }
 
     // AuthenticationManager 설정 (필요 시)
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .build();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 }
